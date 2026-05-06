b8fe7a0419ee9fe66f75bf237db66589dec307a4
jtoribio2 <j.toribio2@sapalomera.cat>
2026-05-06 10:52:04 +0200

    creacion de main de pruebas para cada uno

---
+++ Practica 1/ProjEscJoelDarwin/src/MainD.java
@@ -0,0 +1,19 @@
+import config.AppConfig;
+import config.DAOFactory;
+import controller.SectorController;
+import dao.impl.mysql.SectorMySQLDAO;
+import dao.interfaces.SectorDAO;
+import dao.interfaces.ViaDAO;
+import db.ConnectionFactory;
+import db.ConnectionProvider;
+import model.entity.Escola;
+import model.entity.Via;
+import service.SectorService;
+import model.entity.*;
+import  java.util.*;
+
+public class MainD {
+    public static void main(String[] args) {
+
+    }
+}
