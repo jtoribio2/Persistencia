ad12cdbaf178e76c9efe688f9a27fb2bbd4a7073
Darwin <d.biag@sapalomera.cat>
2026-05-07 09:42:53 +0200

    ESCALDOR COMPROVACION

--- Practica 1/ProjEscJoelDarwin/src/controller/EscaladorController.java
+++ Practica 1/ProjEscJoelDarwin/src/controller/EscaladorController.java
@@ -10,16 +10,22 @@ import java.util.List;
 public class EscaladorController {
 
    private  EscaladorService dao ;
-
+/**
+ * @param s Service
+ * **/
 public EscaladorController(EscaladorService s){this.dao = s;}
     public  void addEscalador(Escalador e ) {
 
-    try{dao.crearEscalador(e);}
+    try{
+        dao.crearEscalador(e);
+    }
     catch (Exception e2 ) {
         System.out.println(e2);
     }
 }
-
+    /**
+     @param e Escalador *
+     **/
     public   void SetEscalador( Escalador e ) {
       try{
         dao.modificarEscalador(e);
@@ -28,25 +34,36 @@ public EscaladorController(EscaladorService s){this.dao = s;}
           System.out.println(e2);
       }
     }
-
+/**
+ * @param id Integer
+ * **/
     public  void removeEscalador(Integer id ) {
         try {
             dao.eliminarEscalador(id);
+            System.out.println("ESCALADOR ELIMINADO");
         }
         catch (Exception e ){
             System.out.println(id);
         }
     }
-
+/**
+ * @param dni String
+ * @return Escalador
+ * **/
     public Escalador getEscaldorDni(String dni){
         try {
-            return dao.obtenerPorDni(dni);
+            Escalador es = dao.obtenerPorDni(dni);
+            System.out.println("ESCALADOR ELIMINAT");
+            return es;
         }
         catch (Exception e ){
+        System.out.println(e);
         return null;
         }
     }
-
+    /**
+     * @param dni String
+     * **/
     public  void removedni(String dni){
        try {
            dao.eliminarPerDni(dni);
@@ -55,21 +72,28 @@ public EscaladorController(EscaladorService s){this.dao = s;}
            System.out.println(e);
        }
     }
-
+    /**
+     * @return List<Escalador> Un arrayList que devuelve Escaldores
+     * **/
     public  List<Escalador> getList(){
-        //Hacer comprovaciones o algo
-        List<Escalador> e = null;
+
+
         try {
-             e = dao.obtenerTodos();
+            List<Escalador> e = dao.obtenerTodos();
+
+            return e;
         }
         catch (Exception e2 ){
-
+            System.out.println(e2);
             return null;
         }
 
-        return e;
-    }
 
+    }
+/**
+ * @param id Integer
+ * @return Escalador
+ * **/
     public  Escalador getEscalador(Integer id  ) {
     try {
         return dao.obtenerPorId(id);
@@ -81,16 +105,18 @@ public EscaladorController(EscaladorService s){this.dao = s;}
     }
 
     public void buscarPorNivel(String dni){
-            List<EscaladorNivellDTO> llista = dao.buscarPorNivell(dni);
-            if (llista.isEmpty()){
-                System.out.println("No hi han escaladors amb el mateix nivell que tu asolit o superior");
-            }
-            else {
-                for (int i = 0; i < llista.size(); i++) {
-                    System.out.println(llista.get(i));
-                }
-            }
-
+           try {
+               List<EscaladorNivellDTO> llista = dao.buscarPorNivell(dni);
+               if (llista.isEmpty()) {
+                   System.out.println("No hi han escaladors amb el mateix nivell que tu asolit o superior");
+               } else {
+                   for (int i = 0; i < llista.size(); i++) {
+                       System.out.println(llista.get(i));
+                   }
+               }
+           }catch (Exception e ){
+               System.out.println(e);
+           }
     }
 
 }
--- Practica 1/ProjEscJoelDarwin/src/service/EscaladorService.java
+++ Practica 1/ProjEscJoelDarwin/src/service/EscaladorService.java
@@ -14,11 +14,16 @@ import java.util.List;
 public class EscaladorService  {
     private final EscaladorDAO escaladorDao;
 
-
+    /**
+     * @param edao COLOCAMOS OBJETO
+     * **/
     public EscaladorService(EscaladorDAO edao) {
         this.escaladorDao = edao;
     }
-    // inserta un sector a la base de datos
+    // insertae EscLADOR
+    /**
+     * @param e ESCALADOR
+     * **/
     public void crearEscalador(Escalador e ) throws Exception {
 
         if (e == null) {
@@ -33,7 +38,7 @@ public class EscaladorService  {
             throw new Exception( "El DNI es INCORRECTE ");
         }
 
-        if(e.getEdat() < 0) throw new Exception("Error edad");
+        if(e.getEdat() < 0 || e.getEdat() > 100) throw new Exception("Error edad");
 
 
         if (e.getEstil() > 0 && e.getEstil() <= 3) {throw new Exception("Debe indicar una estilo válida");}
@@ -42,7 +47,9 @@ public class EscaladorService  {
     }
 
     // devuelve una lista donde aparecen todos los sectores de la bd
-
+    /**
+     *@return Escalador Retorna UNA LLISTA ESCALADOR
+     **/
     public List<Escalador> obtenerTodos() throws  Exception {
 
         List<Escalador> e = escaladorDao.obtindreTots();
@@ -54,6 +61,10 @@ public class EscaladorService  {
     }
 
     // devuelve un sector a traves de su id
+    /**
+     * @param id INT
+     * @return Escaladro retorna el objecte escalador
+     * **/
     public Escalador  obtenerPorId(Integer id ) throws Exception{
 
         if (id < 0) {
@@ -62,17 +73,25 @@ public class EscaladorService  {
 
         return escaladorDao.obtenir(id);
     }
-
+    /**
+     @param dni String*
+     @return Retorna objecte Escalador
+     **/
     public Escalador obtenerPorDni(String dni ) throws Exception{
 
-        if (!dni.matches("^[0-9]{8}[A-Z]$") ) {
+        if (!dni.matches("^[0-9]{8}[A-Z]$") || dni.length() > 9) {
             throw new Exception("DNI INCORRECTE");
         }
         return escaladorDao.obtenirPerDni(dni);
     }
 
-
+    /**
+     * @param dni String
+     *
+     * **/
     public void eliminarPerDni(String dni) throws Exception{
+
+        if(dni.isBlank()){throw new Exception("DNI NO INTRODUIT");}
         if (!dni.matches("^[0-9]{8}][A-Z]$") ) {
             throw new Exception("DNI INCORRECTE");
         }
@@ -80,6 +99,9 @@ public class EscaladorService  {
         escaladorDao.eliminarDni(dni);
     }
     // elimina un sector a traves de un id
+    /***
+     * @param id Int
+     * **/
     public void eliminarEscalador(Integer id) throws Exception {
 
         if (id < 0) {
@@ -90,6 +112,9 @@ public class EscaladorService  {
     }
 
     // modifica un sector y lo busca a traves de su id en el caso que no existe te dice que no ha podido modificarlo
+    /**
+     * @param e Escalador
+     * **/
     public void modificarEscalador(Escalador  e) throws Exception {
 
         if (e == null) {
@@ -118,8 +143,14 @@ public class EscaladorService  {
 
         escaladorDao.modificar(e);
     }
-
-    public List<EscaladorNivellDTO> buscarPorNivell(String dni){
+/**
+ * @param dni String
+ * @return List<EscaladorNivellDTO> Una lista
+ * **/
+    public List<EscaladorNivellDTO> buscarPorNivell(String dni) throws Exception{
+        if (!dni.matches("^[0-9]{8}[A-Z]$") || dni.length() > 9) {
+            throw new Exception("DNI INCORRECTE O LONGITUD SENSE POSAR BE");
+        }
         return escaladorDao.buscarPorNivel(dni);
     }
 
