598880f44ea5dc6d3ce11b7b1bc5c3b97ae911c8
jtoribio2 <j.toribio2@sapalomera.cat>
2026-05-09 13:22:26 +0200

    Cambios en el shquema de la base de datos, arreglo de uniques en cada tabla

---
+++ Practica 1/ProjEscJoelDarwin/src/controller/Menus/BuscarMenu.java
@@ -0,0 +1,92 @@
+package controller.Menus;
+
+import View.MenusView.MenuBuscarView;
+import config.AppConfig;
+
+import java.util.Scanner;
+
+public class BuscarMenu {
+
+    private final Scanner sc = new Scanner(System.in);
+
+    public void iniciar() {
+
+        int opcio;
+
+        do {
+
+            MenuBuscarView.mostrarMenu();
+
+            while (!sc.hasNextInt()) {
+
+                System.out.println("Introdueix un número vàlid");
+                sc.nextLine();
+            }
+
+            opcio = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcio) {
+
+                case 1:
+
+                    System.out.println("Introdueix ID Escalador:");
+
+                    int idEscalador = sc.nextInt();
+                    sc.nextLine();
+
+                    AppConfig
+                            .getEscaladorController()
+                            .;
+
+                    break;
+
+                case 2:
+
+                    System.out.println("Introdueix nom Escola:");
+
+                    String nomEscola = sc.nextLine();
+
+                    AppConfig
+                            .getEscolaController()
+                            .buscarPorNombre(nomEscola);
+
+                    break;
+
+                case 3:
+
+                    System.out.println("Introdueix nom Sector:");
+
+                    String nomSector = sc.nextLine();
+
+                    AppConfig
+                            .getSectorController()
+                            .buscarPorNombre(nomSector);
+
+                    break;
+
+                case 4:
+
+                    System.out.println("Introdueix nom Via:");
+
+                    String nomVia = sc.nextLine();
+
+                    AppConfig
+                            .getViaController()
+                            .buscarPorNombre(nomVia);
+
+                    break;
+
+                case 0:
+
+                    System.out.println("Tornant...");
+                    break;
+
+                default:
+
+                    System.out.println("Opció incorrecta");
+            }
+
+        } while (opcio != 0);
+    }
+}
